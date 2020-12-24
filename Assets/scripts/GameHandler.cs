4c7d0b6dd9bdedecbeddf0ac9d3cbfbe077059d4
Liannium <meliaerinelizabeth@gmail.com>
2020-12-24 15:45:41 -0800

    added game handler object and script

---
+++ Assets/scripts/GameHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    GameObject enemySprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemySprite = GameObject.Find("enemy_sprite");
+        Debug.Log("Found sprite");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
