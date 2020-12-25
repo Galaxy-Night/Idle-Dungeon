061d7a271eac6a043f6181da38706105877672fb
Liannium <meliaerinelizabeth@gmail.com>
2020-12-25 15:33:39 -0800

    Documented code

--- Assets/scripts/EnemyObject.cs
+++ Assets/scripts/EnemyObject.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary><c>EnemyObject</c> represents the enemy prefab that Unity 
+/// interacts with. It contains the interface for the user to interact with 
+/// enemies</summary>
+
 public class EnemyObject : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -16,6 +20,7 @@ public class EnemyObject : MonoBehaviour
 
     }
 
+    // OnMouseDown is called when the user clicks on the object
     private void OnMouseDown()
     {
         SendMessageUpwards("DealEnemyDamage", 1);
--- Assets/scripts/GameHandler.cs
+++ Assets/scripts/GameHandler.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// <c>GameHandler</c> is a class to handle the main functionality of the game.
+/// </summary>
 public class GameHandler : MonoBehaviour
 {
     public GameObject enemyPrefab;
@@ -20,6 +23,13 @@ public class GameHandler : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// <c>DealEnemyDamage</c> is invoked by <c>EnemyObject</c> when the user
+    /// clicks within the enemy's collision box using 
+    /// <c>SendMessageUpwards</c>. <c>DealEnemyDamage</c> is used to ensure the
+    /// game responds properly when an enemy is killed.
+    /// </summary>
+    /// <param name="_amount">The amount of damage the enemy is dealt</param>
     void DealEnemyDamage(int _amount) {
         if (currentEnemy.TakeDamage(_amount))
             Destroy(enemyPrefab);
