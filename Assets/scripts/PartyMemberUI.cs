0564339c5800f062743d3169fc327f7aa3021911
Liannium <meliaerinelizabeth@gmail.com>
2021-03-07 12:53:08 -0800

    removed old code and started new Party Member prefab

---
+++ Assets/scripts/PartyMemberUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyMemberUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject unlockButton;
+    [SerializeField]
+    private GameObject levelButton;
+    [SerializeField]
+    private GameObject currentLevel;
+    [SerializeField]
+    private GameObject levelCost;
+    [SerializeField]
+    private GameObject healCost;
+    [SerializeField]
+    private GameObject healButton;
+    [SerializeField]
+    private GameObject heal;
+    
+    public void OnUnlockClick(Text currentCoins) {
+        
+	}
+
+    public void OnLevelClick(Text currentCoins) {
+
+	}
+
+    public void OnHealClick(Text currentCoins) {
+
+	}
+}
