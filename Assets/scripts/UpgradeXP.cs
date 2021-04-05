fc81621ba587f4eb2673d7e45418e26fe7866e81
Liannium <meliaerinelizabeth@gmail.com>
2021-04-05 16:19:08 -0700

    User can upgrade how much XP they earn

---
+++ Assets/scripts/UpgradeXP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeXP : UpgradeBase
+{
+    protected override void PopulateTiers()
+    {
+        base.PopulateTiers();
+        tiers.Add(new System.Tuple<float, int>(1.1f, 100));
+        tiers.Add(new System.Tuple<float, int>(1.2f, 500));
+        tiers.Add(new System.Tuple<float, int>(1.3f, 1000));
+        tiers.Add(new System.Tuple<float, int>(1.4f, 1500));
+        tiers.Add(new System.Tuple<float, int>(1.5f, 2000));
+    }
+    public override void OnPurchaseClick()
+    {
+        if (checkCoins())
+        {
+            Game handler = GameObject.Find("game_handler").GetComponent<Game>();
+            handler.data.UpgradeXp(multiplier);
+            NextTier();
+            OnBackClick();
+        }
+    }
+    protected override void UpdateDescription()
+    {
+        description = $"Multiplies xp earned by {multiplier}";
+    }
+}
